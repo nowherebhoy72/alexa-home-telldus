64e2f5255b732b55ffcd9c75d97efc87f1de0716
nowherebhoy72 <nowherebhoy72@users.noreply.github.com>
2016-10-29 00:27:01 +0100

    Update Global.groovy

--- src/main/groovy/Global.groovy
+++ src/main/groovy/Global.groovy
@@ -1,6 +1,6 @@
 class Global {
-  static String publictoken = ''
-  static String secrettoken = ''
-  static String publickey =  ''
-  static String secretkey =  ''
+  static String publictoken = FEHUVEW84RAFR5SP22RABURUPHAFRUNU
+  static String secrettoken = ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF
+  static String publickey =  918861369c82edfcb2de978c95faf7c705813dc84
+  static String secretkey =  4c109ba2c8c1140dcab01026474b446e
 }
